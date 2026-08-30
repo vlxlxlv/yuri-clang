cdc4c193efed08fa0098de39e490272d88114dc0
vlxlxlv <fajarslebew31@gmail.com>
2026-08-30 18:37:42 +0700

    Import 2026-08-30 Build * Clang 23.1.0 taken from branch `llvmorg-23.1.0`
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

---
+++ include/llvm/IR/IntrinsicsAVR.h
@@ -0,0 +1,27 @@
+/*===- TableGen'erated file -------------------------------------*- C++ -*-===*\
+|*                                                                            *|
+|* Intrinsic Function Source Fragment                                         *|
+|*                                                                            *|
+|* Automatically generated file, do not edit!                                 *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
+
+#ifndef LLVM_IR_INTRINSIC_AVR_ENUMS_H
+#define LLVM_IR_INTRINSIC_AVR_ENUMS_H
+
+namespace llvm::Intrinsic {
+
+enum AVRIntrinsics : unsigned {
+// Enum values for intrinsics.
+    avr_cli = 4235,                                   // llvm.avr.cli (IntrinsicsAVR.td:18)
+    avr_nop,                                   // llvm.avr.nop (IntrinsicsAVR.td:16)
+    avr_sei,                                   // llvm.avr.sei (IntrinsicsAVR.td:17)
+    avr_sleep,                                 // llvm.avr.sleep (IntrinsicsAVR.td:19)
+    avr_swap,                                  // llvm.avr.swap (IntrinsicsAVR.td:23)
+    avr_wdr,                                   // llvm.avr.wdr (IntrinsicsAVR.td:20)
+}; // enum
+
+} // namespace llvm::Intrinsic
+
+#endif // LLVM_IR_INTRINSIC_AVR_ENUMS_H
+
---
+++ include/llvm/Support/VCSRevision.h
@@ -0,0 +1,2 @@
+#define LLVM_REVISION R"(ea7d852a70e8bdfaf601d6626a760f9771b2c4b4)"
+#define LLVM_REPOSITORY R"(https://github.com/llvm/llvm-project.git)"
